fd0874287d8612024dc527516bc983cb439dcfe3
Distortions81 <carlotto81@gmail.com>
2024-09-17 13:31:54 -0600

    snapping

--- editor/main.go
+++ editor/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -12,65 +13,69 @@ import (
 	"golang.org/x/image/colornames"
 )
 
+const snapDistance = 10
+
 // Define a struct for a 2D vector with start and end points
 type Vector2D struct {
 	X1, Y1, X2, Y2 float64
+}
+
+type XY struct {
+	X, Y float64
 }
 
 // Game struct to hold game state
 type Game struct {
 	vectors []Vector2D
 
-	cameraX        float64
-	cameraY        float64
-	startX, startY float64
-
-	lastMouseX, lastMouseY int
+	camera,
+	start,
+	lastMouse XY
 
 	createMode,
 	firstClick,
-	secondClick,
-	dragging bool
+	secondClick bool
 }
 
 // Update is called every frame
 func (g *Game) Update() error {
 	mouseX, mouseY := ebiten.CursorPosition()
+	mpos := XY{X: float64(mouseX), Y: float64(mouseY)}
 
 	if ebiten.IsKeyPressed(ebiten.KeyC) && !g.createMode {
 		g.createMode = true
 		g.firstClick = false
 		g.secondClick = false
-		g.startX = 0
-		g.startY = 0
+		g.start = XY{X: 0, Y: 0}
 	} else if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
 		if g.createMode {
 			if !g.secondClick && !g.firstClick {
+				snappedPos := SnapPosition(mpos, g.vectors, snapDistance)
+
 				// Start creating a new vector
-				g.startX = float64(mouseX) - g.cameraX
-				g.startY = float64(mouseY) - g.cameraY
+				g.start = snappedPos
 				g.firstClick = true
 			} else if g.firstClick && !g.secondClick {
 				// Finish creating the vector
-				endX := float64(mouseX) - g.cameraX
-				endY := float64(mouseY) - g.cameraY
+				endX := float64(mouseX) - g.camera.X
+				endY := float64(mouseY) - g.camera.Y
 				g.vectors = append(g.vectors, Vector2D{
-					X1: g.startX,
-					Y1: g.startY,
+					X1: g.start.X,
+					Y1: g.start.Y,
 					X2: endX,
 					Y2: endY,
 				})
-				fmt.Printf("created: %v,%v - %v,%v\n", g.startX, g.startY, endX, endY)
+				fmt.Printf("created: %v,%v - %v,%v\n", g.start.X, g.start.Y, endX, endY)
 				g.secondClick = true
 				g.createMode = false
 			}
 		}
 	} else if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
-		g.cameraX += float64(mouseX - int(g.lastMouseX))
-		g.cameraY += float64(mouseY - int(g.lastMouseY))
+		g.camera.X += float64(mouseX - int(g.lastMouse.X))
+		g.camera.Y += float64(mouseY - int(g.lastMouse.Y))
 	}
 
-	g.lastMouseX, g.lastMouseY = mouseX, mouseY
+	g.lastMouse = mpos
 	return nil
 }
 
@@ -85,16 +90,18 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	// Draw each vector with respect to the camera position
 	for _, vec := range g.vectors {
-		x1 := vec.X1 + g.cameraX
-		y1 := vec.Y1 + g.cameraY
-		x2 := vec.X2 + g.cameraX
-		y2 := vec.Y2 + g.cameraY
+		x1 := vec.X1 + g.camera.X
+		y1 := vec.Y1 + g.camera.Y
+		x2 := vec.X2 + g.camera.X
+		y2 := vec.Y2 + g.camera.Y
 		vector.StrokeLine(vectorScreen, float32(x1), float32(y1), float32(x2), float32(y2), 1, color.White, true)
 	}
 
 	if g.createMode && g.firstClick && !g.secondClick {
 		mouseX, mouseY := ebiten.CursorPosition()
-		vector.StrokeLine(screen, float32(g.startX), float32(g.startY), float32(mouseX), float32(mouseY), 1, colornames.Red, true)
+		mpos := XY{X: float64(mouseX), Y: float64(mouseY)}
+		snappedPos := SnapPosition(mpos, g.vectors, snapDistance)
+		vector.StrokeLine(screen, float32(g.start.X), float32(g.start.Y), float32(snappedPos.X), float32(snappedPos.Y), 1, colornames.Red, true)
 	}
 
 	// Draw the vector image onto the screen
@@ -121,4 +128,34 @@ func main() {
 	if err := ebiten.RunGame(game); err != nil {
 		panic(err)
 	}
+}
+
+func distance(p1, p2 XY) float64 {
+	return math.Sqrt((p1.X-p2.X)*(p1.X-p2.X) + (p1.Y-p2.Y)*(p1.Y-p2.Y))
+}
+
+// SnapPosition snaps a new position to the nearest existing position within a threshold
+func SnapPosition(newPos XY, existingPositions []Vector2D, threshold float64) XY {
+	var snappedPosition XY = newPos
+	minDistance := threshold // Initialize with threshold to ensure snapping only within the threshold
+
+	for _, pos := range existingPositions {
+
+		apos := XY{X: pos.X1, Y: pos.Y1}
+		bpos := XY{X: pos.X2, Y: pos.Y2}
+
+		dist := distance(newPos, apos)
+		if dist < minDistance {
+			minDistance = dist
+			snappedPosition = apos
+		}
+
+		dist = distance(newPos, bpos)
+		if dist < minDistance {
+			minDistance = dist
+			snappedPosition = bpos
+		}
+	}
+
+	return snappedPosition
 }
